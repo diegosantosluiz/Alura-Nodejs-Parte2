afddf56c81154d86ea2df558ad2a995bb47e9c1d
Diego Santos Luiz <diego.santos.luiz@gmail.com>
2020-01-26 23:31:47 -0300

    Aula 04 - Atividade 05: Implementando a autenticação

--- casadocodigo/src/app/controladores/base-controlador.js
+++ casadocodigo/src/app/controladores/base-controlador.js
@@ -1,4 +1,6 @@
+const LivroControlador = require('./livro-controlador');
 const templates = require('../views/template');
+const passport = require('passport');
 
 class BaseControlador {
 
@@ -14,7 +16,7 @@ class BaseControlador {
       resp.marko(
         templates.base.home
       );
-    }
+    };
   }
 
   login() {
@@ -22,13 +24,29 @@ class BaseControlador {
       resp.marko(
         templates.base.login
       )
-    }
+    };
   }
 
   efetuaLogin() {
-    return function(req, resp) {
-      
-    }
+    return function(req, resp, next) {
+      passport.authenticate('local', (erro, usuario, info) => {
+        if (info) {
+          return resp.marko(templates.base.login);
+        }
+
+        if (erro) {
+          return next(erro);
+        }
+
+        req.login(usuario, (erro) => {
+          if (erro) {
+            return next(erro);
+          }
+
+          return resp.redirect(LivroControlador.rotas().lista);
+        });
+      })(req, resp, next);
+    };
   }
 }
 
